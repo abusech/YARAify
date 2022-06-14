47f41b1b2591b1173a443895281b4223490eb327
abuse.ch <41051806+abusech@users.noreply.github.com>
2022-06-14 11:15:45 +0200

    Update yaraify_list_tasks.py

--- yaraify_list_tasks.py
+++ yaraify_list_tasks.py
@@ -2,13 +2,20 @@
 import requests
 import urllib3
 import json
+import sys
+
+if len(sys.argv) > 1:
+    file = sys.argv[1]
+else:
+    print("Usage: python3 yaraify_list_tasks.py <identifier>")
+    quit()
 
 pool = urllib3.HTTPSConnectionPool('yaraify-api.abuse.ch', port=443, maxsize=50)
 
 data = {
     'query':            'list_tasks',
-    'identifier':        'YOUR-IDENTIFIER', # Your identifier
-    'task_status':	 'queued'
+    'identifier':       sys.argv[1],
+    'task_status':	    'queued'
 }
 json_data = json.dumps(data)
 response = pool.request("POST", "/api/v1/", body=json_data)
