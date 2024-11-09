c89f7135dceb20bb4c2d98daec93bd2d2818fb67
abuse.ch <41051806+abusech@users.noreply.github.com>
2024-11-09 18:48:45 +0100

    Update yaraify_check_taskid.py

--- yaraify_check_taskid.py
+++ yaraify_check_taskid.py
@@ -4,16 +4,22 @@ import urllib3
 import json
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: python3 yaraifier_check_taskid.py <task_id>")
+if len(sys.argv) < 3:
+    print("Query YARAify for a task ID")
+    print("Usage: python3 yaraifier_check_taskid.py <YOUR-AUTH-KEY> <task_id>")
+    print("Note: If you don't have an Auth-Key yet, you can obtain one at https://auth.abuse.ch/")
     quit()
 
-pool = urllib3.HTTPSConnectionPool('yaraify-api.abuse.ch', port=443, maxsize=50)
+headers = {
+    "Auth-Key"      :   sys.argv[1]
+}
+
+pool = urllib3.HTTPSConnectionPool('yaraify-api.abuse.ch', port=443, maxsize=50, headers=headers)
 
 data = {
     'query':            'get_results',
     'malpedia-token':   'xxx', # Your malpedia token. If you don't have any, simply obmit this field or keep it empty
-    'task_id':          sys.argv[1]
+    'task_id':          sys.argv[2]
 }
 json_data = json.dumps(data)
 response = pool.request("POST", "/api/v1/", body=json_data)
